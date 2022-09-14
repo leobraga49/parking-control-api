e35d862fbff17238bfa116ecd983a93305d779ad
leobraga49 <leonardobarrosbraga@gmail.com>
2022-09-14 09:09:18 -0300

    Implementation of the save method

--- src/main/java/com/api/parkingcontrol/service/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/service/ParkingSpotService.java
@@ -1,7 +1,9 @@
 package com.api.parkingcontrol.service;
 
+import com.api.parkingcontrol.model.ParkingSpotModel;
 import com.api.parkingcontrol.repository.ParkingSpotRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ParkingSpotService {
@@ -13,4 +15,17 @@ public class ParkingSpotService {
     }
 
 
+    @Transactional
+    public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
+        return parkingSpotRepository.save(parkingSpotModel);
+    }
+    public boolean existsByLicensePlateCar(String licensePlateCar) {
+        return parkingSpotRepository.existsByLicensePlateCar(licensePlateCar);
+    }
+    public boolean existsByParkingSpotNumber(String parkingSpotNumber) {
+        return parkingSpotRepository.existsByParkingSpotNumber(parkingSpotNumber);
+    }
+    public boolean existsByApartmentAndBlock(String apartment, String block) {
+        return parkingSpotRepository.existsByApartmentAndBlock(apartment, block);
+    }
 }
